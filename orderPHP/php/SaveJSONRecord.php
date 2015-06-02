69adc8ba4c1609b584fbb75148eebccaed4c5a54
Takashi Masuda <xellos00195@gmail.com>
2015-06-02 19:47:11 +0900

    saveJSONRecord.php更新しました

--- orderPHP/php/SaveJSONRecord.php
+++ orderPHP/php/SaveJSONRecord.php
@@ -1,4 +1,5 @@
 <?php
+// データベースに接続するための値を定数として宣言する
 
 // JSONDBManagerクラスファイルを読み込む
 require_once("JSONDBManager.php");
@@ -12,9 +13,9 @@ $jdbm = new JSONDBManager();
 //SQLによる例外の対処のためtryブロックで囲む
 try {
 	// データベースに接続する
-	$this->dbh = new PDO(DSN, DB_USER, DB_PASSWORD);
+	$jdbm->dbh = new PDO(DSN, DB_USER, DB_PASSWORD);
 	// データベースをUTF8で設定する
-	$this->dbh->query('SET NAMES utf8');
+	$jdbm->dbh->query('SET NAMES utf8');
 	//クライアントから送信されたJSON文字列を連想配列に変換する
 	$jdbm->getJSONMap($json);
 	//INSERT、またはUPDATE命令を実行する
@@ -41,10 +42,8 @@ try {
 }
 
 //クライアントへ返すメッセージを作成する。
-$returnMessage = "{\"message\":\"" . $jdbm->processedRecords . "件のレコードを操作しました。\"}";
-// 送信する文字列をjson形式に変換して変数に入れる
-$jsonOut = json_encode($returnMessage, JSON_UNESCAPED_UNICODE);
+$returnMessage = '{"message":"' . $jdbm->processedRecords . '件のレコードを操作しました。"}';
 
 // 作成したJson文字列を出力する
-print($jsonOut);
+print($returnMessage);
 
